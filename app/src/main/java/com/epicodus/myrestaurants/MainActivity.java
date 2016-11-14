4dc852f16980d705bd73b38f50df2ae522d0ea83
Ewa Manek and Zachary Matthewstearn <ewa+zachary@>
2016-11-14 15:33:03 -0800

    Add list of restaurants and custom list item layout

---
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -0,0 +1,42 @@
+package com.epicodus.myrestaurants;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface boopeeFont = Typeface.createFromAsset(getAssets(), "fonts/boopee.ttf");
+        mAppNameTextView.setTypeface(boopeeFont);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
